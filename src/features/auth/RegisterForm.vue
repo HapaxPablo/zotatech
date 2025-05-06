5020dca263f52e9ef09a0fd5adc26782e20a130b
Zabor <pahapasha11@gmail.com>
2025-05-06 23:56:24 +0700

    wip  - style register form

--- src/features/auth/RegisterForm.vue
+++ src/features/auth/RegisterForm.vue
@@ -1,41 +1,53 @@
 <template>
   <form @submit.prevent="onSubmit" class="register-form">
-    <div>
-      <label>Логин *</label>
-      <input v-model="form.login" type="text" />
-      <span v-if="errors.login">{{ errors.login }}</span>
-    </div>
+    <div class="text-2xl font-bold">Регистрация</div>
+    <div class="form-fields">
+      <div>
+        <label>Логин *</label>
+        <input v-model="form.login" type="text" class="border-none" />
+        <span v-if="errors.login" class="error">{{ errors.login }}</span>
+      </div>
 
-    <div>
-      <label>Email</label>
-      <input v-model="form.email" type="email" />
-      <span v-if="errors.email">{{ errors.email }}</span>
-    </div>
+      <div>
+        <label>Email</label>
+        <input v-model="form.email" type="email" />
+        <span v-if="errors.email" class="error">{{ errors.email }}</span>
+      </div>
 
-    <div>
-      <label>Телефон</label>
-      <input v-model="form.phone" v-mask="'+7 (###) ###-##-##'" type="tel" />
-      <span v-if="errors.phone">{{ errors.phone }}</span>
-    </div>
+      <div>
+        <label>Телефон</label>
+        <input v-model="form.phone" v-mask="'+7 (###) ###-##-##'" type="tel" />
+        <span v-if="errors.phone" class="error">{{ errors.phone }}</span>
+      </div>
 
-    <div>
-      <label>Пароль *</label>
-      <input v-model="form.password" type="password" />
-      <span v-if="errors.password">{{ errors.password }}</span>
-    </div>
+      <div>
+        <label>Пароль *</label>
+        <input v-model="form.password" type="password" />
+        <span v-if="errors.password" class="error">{{ errors.password }}</span>
+      </div>
 
-    <div>
-      <label>Подтверждение пароля *</label>
-      <input v-model="form.confirmPassword" type="password" />
-      <span v-if="errors.confirmPassword">{{ errors.confirmPassword }}</span>
+      <div>
+        <label>Подтверждение пароля *</label>
+        <input v-model="form.confirmPassword" type="password" />
+        <span v-if="errors.confirmPassword" class="error">{{ errors.confirmPassword }}</span>
+      </div>
     </div>
-
     <!-- <button type="submit">Зарегистрироваться</button> -->
-    <CustomButton
-      text="Зарегистрироваться"
-      type="submit"
-      className="bg-[#2B2A29] text-white w-full h-[50px]"
-    />
+    <div class="h-[50px] mt-[42px]">
+      <CustomButton
+        text="Зарегистрироваться"
+        type="submit"
+        className="bg-[#2B2A29] text-white w-full h-full"
+      />
+    </div>
+
+    <div>
+      <div class="border-t" />
+      <span class="font-normal text-[14px] leading-[18px] text-[#878787]">
+        Нажимая кнопку «Зарегистрироваться», пользователь соглашается с политикой в отношении
+        обработки персональных данных и публичной офертой
+      </span>
+    </div>
   </form>
 </template>
 
@@ -89,9 +101,18 @@ const onSubmit = () => {
   padding: 42px;
   display: flex;
   flex-direction: column;
-  gap: 1rem;
-  width: 50%;
+  max-width: 614px;
+  max-height: 701px;
+  height: 100vh;
+  width: 100%;
   background-color: #fffffe;
+  justify-content: space-between;
+}
+
+.form-fields {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
 }
 
 label {
@@ -99,12 +120,29 @@ label {
 }
 
 input {
-  padding: 0.5rem;
-  border: 1px solid #ccc;
+  width: 100%;
+  padding: 8px 0;
+  border: none;
+  border-bottom: 1px solid #222;
+  background: transparent;
+  color: #222;
+  font-size: 1rem;
+  border-radius: 0;
+  outline: none;
+  transition: border-color 0.2s;
 }
 
-span {
+input:focus {
+  border-bottom: 2px solid #2b2a29;
+}
+
+.error {
   color: red;
   font-size: 0.9rem;
+}
+
+.border-t {
+  border-top: 1px solid #ccc;
+  margin: 1rem 0;
 }
 </style>
